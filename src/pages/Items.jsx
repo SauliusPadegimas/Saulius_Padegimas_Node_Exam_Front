66d93cc252c380502dc324662d8a7eb42b95255c
SauliusPadegimas <saulius.padegimas@gmail.com>
2022-11-21 10:45:44 +0200

    filters implement

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -5,7 +5,50 @@ import MainContext from '../components/MainContext';
 
 function Items() {
   const [loading, setLoading] = useState(true);
-  const { socket, items, setItems } = useContext(MainContext);
+  const { socket, items, setItems, timeNow } = useContext(MainContext);
+  const [filteredItems, setFilteredItems] = useState(items);
+  const [isChecked, setIsChecked] = useState(false);
+  const [query, setQuery] = useState('');
+
+  function handleCheck() {
+    setIsChecked(!isChecked);
+  }
+
+  function handleSearch() {
+    if (query.length > 0) {
+      const filteredArr = items.filter((x) => {
+        const name = x.title.toLowerCase();
+        const search = query.toLowerCase().trim();
+        if (name.includes(search)) {
+          return true;
+        } else return false;
+      });
+      return filteredArr;
+    } else return items;
+  }
+
+  useEffect(() => {
+    setFilteredItems(items);
+  }, [items]);
+
+  useEffect(() => {
+    if (isChecked) {
+      let filteredArr = handleSearch();
+      filteredArr = filteredArr.filter((x) => {
+        let endTime = new Date(x.date).getTime();
+        const timeLeft = endTime - timeNow;
+        if (timeLeft <= 0) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      setFilteredItems(filteredArr);
+    } else {
+      let filteredArr = handleSearch();
+      setFilteredItems(filteredArr);
+    }
+  }, [isChecked, timeNow, query]);
 
   useEffect(() => {
     socket.emit('items');
@@ -20,17 +63,34 @@ function Items() {
     return <Loading />;
   } else
     return (
-      <div className='items__container fitVH'>
-        {items.map((x) => (
-          <Item
-            key={x._id}
-            id={x._id}
-            photo={x.photo}
-            title={x.title}
-            bids={x.bids}
-            date={x.date}
+      <div className='items__page'>
+        <div className='items__filter'>
+          <input type='checkbox' id='endedCheck' checked={isChecked} onChange={handleCheck} />
+          <label htmlFor='endedCheck' className='filter-label'>
+            Tik nesibaigę
+          </label>
+          <br />
+          <label htmlFor='searchInput' className='filter-label'>
+            Paieška
+          </label>
+          <input
+            type='search'
+            id='searchInput'
+            onChange={(event) => setQuery(event.target.value)}
           />
-        ))}
+        </div>
+        <div className='items__container fitVH'>
+          {filteredItems.map((x) => (
+            <Item
+              key={x._id}
+              id={x._id}
+              photo={x.photo}
+              title={x.title}
+              bids={x.bids}
+              date={x.date}
+            />
+          ))}
+        </div>{' '}
       </div>
     );
 }
